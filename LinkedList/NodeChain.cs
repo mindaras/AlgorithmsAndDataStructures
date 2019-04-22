8ce135e9fc7d7194c6efc4aba1a646b49c6034fd
Mindaugas Lazauskas <mind.lazauskas@gmail.com>
2019-04-22 11:44:20 +0300

    LinkedList and DoublyLinkedList implementation

---
+++ LinkedList/NodeChain.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LinkedList
+{
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node Next { get; set; }
+    }
+
+    public class NodeChain
+    {
+        public NodeChain()
+        {
+            var first = new Node { Value = 3 };
+            var middle = new Node { Value = 5 };
+
+            first.Next = middle;
+
+            var last = new Node { Value = 7 };
+
+            middle.Next = last;
+
+            PrintList(first);
+        }
+
+        public void PrintList(Node node)
+        {
+            while (node != null)
+            {
+                Console.WriteLine(node.Value);
+                node = node.Next;
+            }
+        }
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using DoublyLinkedList;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
