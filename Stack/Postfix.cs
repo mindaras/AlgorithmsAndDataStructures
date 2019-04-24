008f6bc56e2935da65d7b3def30ae89a36fa0ea4
Mindaugas Lazauskas <mind.lazauskas@gmail.com>
2019-04-24 06:27:10 +0300

    stack implementation using a linked list and array as the backing store

---
+++ Stack/Postfix.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stack
+{
+    public class Postfix
+    {
+        Stack<int> values = new Stack<int>();
+
+        public int Calculate(string[] args)
+        {
+            foreach (string token in args)
+            {
+                if (int.TryParse(token, out int value))
+                {
+                    values.Push(value);
+                }
+                else
+                {
+                    int rhs = values.Pop();
+                    int lhs = values.Pop();
+
+                    switch (token)
+                    {
+                        case "+":
+                            values.Push(lhs + rhs);
+                            break;
+                        case "-":
+                            values.Push(lhs - rhs);
+                            break;
+                        case "*":
+                            values.Push(lhs * rhs);
+                            break;
+                        case "/":
+                            values.Push(lhs / rhs);
+                            break;
+                        case "%":
+                            values.Push(lhs % rhs);
+                            break;
+                        default:
+                            throw new ArgumentException($"Unrecognized token: {token}");
+                    }
+                }
+            }
+
+            return values.Pop();
+        }
+    }
+}
